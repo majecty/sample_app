3b933ee38962ce70b49cd0d7f05108b0f8bae3f6
majecty <majecty@majectyserver.snu.ac.kr>
2011-04-06 01:21:30 +0900

    시작합니다.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'bfb801c4fda01443dda5664842261408598a52457014d4c5efaac502dad5d387e89f4273cc49d4c3b486ddf79542dd499b89b9bb02c8fe882f88246a57bec4fc'
