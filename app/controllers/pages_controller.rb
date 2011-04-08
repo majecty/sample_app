0696a153344d30f3f80153896d3da09ef0be252e
majecty <majecty@majectyserver.snu.ac.kr>
2011-04-08 23:43:24 +0900

    Remove default Page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,4 +14,8 @@ class PagesController < ApplicationController
   def help
     @title = "Help"
   end
+
+  def test
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
-  get "pages/contact"
+  root :to => 'pages#home'
 
-  get "pages/about"
-  get "pages/help"
+#  get "pages/home"
+
+#  get "pages/contact"
+
+#  get "pages/about"
+#  get "pages/help"
+#  get "pages/test"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
